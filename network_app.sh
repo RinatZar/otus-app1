2afaf2012e8520bca56889a7022c435a7b38cf44
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-03-25 17:43:36 +0300

    Update network_app.sh

--- network_app.sh
+++ network_app.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-DIR='/etc/systemd/network/'
-DIR1='/home/otus-app1/02-eth.network'
-DIR2='/etc/systemd/network/'
+DIR='/etc/netplan/'
+DIR1='/home/otus-app1/02-static.yml'
+DIR2='/etc/netplan/'
 
 #Копируем конфиг 
 cp $DIR1 $DIR2
-echo "файл конфига 02-eth.network скопирован в $DIR"
+echo "файл конфига 02-static.yml скопирован в $DIR"
+
+netplan --debug generate
+netplan apply
+ip -4 a
+
+
+
 
 #Перезапускаем сервис networkd
-systemctl restart systemd-networkd
-if [ $? -eq 0 ]; then
-          echo "Статический IP включен"
-else
-          echo "Возникла ошибка"
-fi
+#systemctl restart systemd-networkd
+#if [ $? -eq 0 ]; then
+#          echo "Статический IP включен"
+#else
+#          echo "Возникла ошибка"
+#fi
