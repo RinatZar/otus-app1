f8b6edf1840c09867b0a036e0c2121b41f308f61
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-04 18:15:44 +0300

    Update nginx_script.sh

--- nginx_script.sh
+++ nginx_script.sh
@@ -2,12 +2,14 @@
 
 DIR='/etc/nginx/sites-enabled/'
 DIR1='/home/otus-app1/default'
+DIR3='/home/otus-app1/status.conf'
 DIR2='/etc/nginx/sites-enabled/'
 
 #Копируем конфиг 
 cp $DIR1 $DIR2
-echo "файл конфига default скопирован в $DIR"
-systemctl reload nginx
+cp $DIR3 $DIR2
+echo "файл конфиги скопированы в $DIR"
+systemctl restart nginx
 if [ $? -eq 0 ]; then
           echo "Nginx запущен, готов к работе"
 else
